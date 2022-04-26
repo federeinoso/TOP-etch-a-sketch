6c732e1f9abfd0c66856b8888705bae32eaae85b
Federico <federeinoso01@gmail.com>
2022-04-26 02:11:55 -0300

    test js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let container = document.getElementById("container");
 
+// GRID FUNCTION
+
 function makeGrid(size) {
   let number = size;
   for (i = 0; i < size * size; i++) {
@@ -9,8 +11,35 @@ function makeGrid(size) {
       `repeat(${number}, 50px)`
     );
     div.classList.add("divGrid");
+    div.addEventListener("mousedown", function () {
+      div.style.backgroundColor = "black";
+    });
     container.appendChild(div);
   }
 }
 
-makeGrid(9);
+window.onload = () => {
+  makeGrid(6);
+};
+
+// RESET FUNCTION
+
+function reset() {
+  container.innerHTML = "";
+  makeGrid(6);
+}
+
+let btnReset = document.getElementById("btnReset");
+btnReset.addEventListener("click", reset);
+
+//INPUT
+let input = document.querySelector("input");
+let gridNumber;
+input.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    gridNumber = input.value;
+    container.innerHTML = "";
+    makeGrid(gridNumber);
+  }
+});
